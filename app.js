b24ceabcc754dd6f416da467ed176a347353d6b7
Dergach Roman <dergachroma@gmail.com>
2022-04-12 14:54:38 +0300

    подключил все необходимое для проекта

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("In Node We Trust");
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+const app = express();
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
