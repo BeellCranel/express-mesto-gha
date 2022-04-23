2f964880940dcb401d58a26312fe4a1b1fda971a
Dergach Roman <dergachroma@gmail.com>
2022-04-23 17:22:31 +0300

    .

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,4 +34,17 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) throw new UnauthorizedError('Неправильная почта или пароль');
+      return bcrypt.compare(password, user.password)
+        .then((isValidPassword) => {
+          if (!isValidPassword) throw new UnauthorizedError('Неправильная почта или пароль');
+          return user;
+        });
+    });
+};
+
 module.exports = mongoose.model('user', userSchema);
